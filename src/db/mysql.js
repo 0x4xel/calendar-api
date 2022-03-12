23b1575cdc8d184f4bf98ef1e37c61877700b524
axel <axel@wunderapp.es>
2022-03-12 20:01:55 +0100

    Actualizado version Sequelize Agregado definicion del modelo

--- src/db/mysql.js
+++ src/db/mysql.js
@@ -1,32 +1,35 @@
 const Sequelize = require('sequelize');
 
-
-
+const AlumnoModel = require("../models/alumno.model");
 const db = {}; 	// Inicializo la base de datos
 
 const sequelize = new Sequelize(
 	process.env.DB_NAME,
 	process.env.DB_USERNAME,
-	"",
+	process.env.DB_PASSWORD,
 	{
 	  host: process.env.DB_HOST,
 	  port: process.env.DB_PORT,
 	  dialect: process.env.DB_DIALECT,
-	  logging: process.env.DB_LOGGING !== true ? console.log("alo") : false,
+	  logging: process.env.DB_LOGGING !== true ? console.log("Logueado con exito") : false,
 	  benchmark: true,
 	  pool: {
-		max: process.env.DB_POOL_MAX,
-		min: process.env.DB_POOL_MIN,
 		idle: process.env.DB_CONNECTION_IDLE,
 	  },
-	  operatorsAliases: false, // to supress the deprecation warning
+
 	},
 );
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
+Alumno = AlumnoModel(sequelize, Sequelize);
 
+
+
+db.Alumno = Alumno;
 module.exports = db;
+
+  
 
 
