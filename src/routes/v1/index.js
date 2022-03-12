f931ed147ec1e10fb5170f6e3a2b54bc995f88f7
axel <axel@wunderapp.es>
2022-03-12 20:53:13 +0100

    Ruta Cursos

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,7 @@
 const allRoutes = require('express').Router();
 const userRoutes = require('../../modules/users/users.routes');
 const alumnoRouter = require('../../modules/alumno/alumno.routes');
+const cursoRouter = require('../../modules/curso/curso.routes');
 
 
 // Rutas de cada modelo. Se utilizará un handler de cada Ruta del modelo (CRUD)
@@ -9,5 +10,6 @@ allRoutes.use(userRoutes);
 // allRoutes.use(examRouter);
 // allRoutes.use(signatureRouter);
 allRoutes.use(alumnoRouter);
+allRoutes.use(cursoRouter);
 
 module.exports = allRoutes;
