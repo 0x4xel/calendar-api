4de65a3aef317ae75f5e78cd4eff70aa4ed0453a
axel <axel@wunderapp.es>
2022-03-18 20:33:03 +0100

    añadidos ruters de carrera

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,16 +4,18 @@ const alumnoRouter = require('../../modules/alumno/alumno.routes');
 const cursoRouter = require('../../modules/curso/curso.routes');
 const asignaturaRouter = require('../../modules/asignatura/asignatura.routes');
 const horaRouter = require('../../modules/hora/hora.routes');
+const asignaturaHoraRouter = require('../../modules/asignaturaHora/asignaturaHora.routes');
+const carreraRouter = require('../../modules/carrera/carrera.routes');
 
 
 // Rutas de cada modelo. Se utilizará un handler de cada Ruta del modelo (CRUD)
 allRoutes.use(userRoutes);
 
-// allRoutes.use(examRouter);
-// allRoutes.use(signatureRouter);
 allRoutes.use(alumnoRouter);
 allRoutes.use(cursoRouter);
 allRoutes.use(asignaturaRouter);
 allRoutes.use(horaRouter);
+allRoutes.use(asignaturaHoraRouter);
+allRoutes.use(carreraRouter);
 
 module.exports = allRoutes;
